07e1eb925c23d1e337166603077386bf5cf17b4a
Valeriya Subocheva <96348645+SubochevaValeriya@users.noreply.github.com>
2023-05-05 16:13:13 +0300

    initial

--- pkg/random_fact.go
+++ pkg/random_fact.go
@@ -3,10 +3,17 @@ package randoms
 import (
 	"io"
 	"net/http"
+	"os"
 )
 
 func RandomFact() (string, error) {
-	resp, err := http.Get("https://api.api-ninjas.com/v1/facts?limit=1")
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "https://api.api-ninjas.com/v1/facts?limit=1", nil)
+	if err != nil {
+		return "", err
+	}
+	req.Header.Set("X-Api-Key", os.Getenv("API_FACT_KEY"))
+	resp, err := client.Do(req)
 	defer resp.Body.Close()
 	if err != nil {
 		return "", err
