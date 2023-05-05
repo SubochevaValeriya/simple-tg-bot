d52ce9376bf65ca58025ea0fbda12d150cf4da5f
Valeriya Subocheva <96348645+SubochevaValeriya@users.noreply.github.com>
2023-05-05 17:34:54 +0300

    Refactoring

--- api/handler.go
+++ api/handler.go
@@ -19,15 +19,6 @@ func init() {
 	_, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 	bot = telegram.NewBot(http.Client{}, os.Getenv("BOT_TOKEN"))
-	//err := bot.Start(ctx)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//var err error
-	//bot, err = tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
-	//if err != nil {
-	//	log.Panic(err)
-	//}
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
@@ -44,79 +35,4 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(update)
 	fmt.Println(body)
 	bot.SendMessage(context.Background(), update)
-
-	//var update tgbotapi.Update
-	//
-	//err = json.Unmarshal(body, &update)
-	//if err != nil {
-	//	log.Println(err)
-	//	return
-	//}
-	//
-	//ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-	//defer stop()
-	//
-	//bot := mybot.NewBot(http.Client{}, os.Getenv("MYVKBOT_TOKEN"))
-	//err := bot.Start(ctx)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//
-	//
-	//log.Println("why")
-	//
-	//if update.Message != nil {
-	//	// Construct a new message from the given chat ID and containing
-	//	// the text that we received.
-	//	msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-	//
-	//	// If the message was open, add a copy of our numeric keyboard.
-	//	switch update.Message.Text {
-	//	case "open":
-	//		msg.ReplyMarkup = firstLineButtons
-	//
-	//	}
-	//
-	//	// Send the message.
-	//	if _, err = bot.Send(msg); err != nil {
-	//		log.Fatal(err)
-	//	}
-	//}
-	//
-	//if update.Message.Text != "" {
-	//	log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-	//
-	//	//telegram.SendMsg(update, bot)
-	//}
-	//
-	//if update.CallbackQuery != nil {
-	//	// Respond to the callback query, telling Telegram to show the user
-	//	// a message with the data received.
-	//	callback := tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data)
-	//	if _, err := bot.Request(callback); err != nil {
-	//		log.Fatal(err)
-	//	}
-	//
-	//	// And finally, send a message containing the data received.
-	//	msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Data)
-	//	if _, err := bot.Send(msg); err != nil {
-	//		log.Fatal(err)
-	//	}
-	//}
 }
-
-//var firstLineButtons = tgbotapi.NewInlineKeyboardMarkup(
-//	tgbotapi.NewInlineKeyboardRow(
-//		tgbotapi.NewInlineKeyboardButtonData("random fact", "random fact"),
-//		tgbotapi.NewInlineKeyboardButtonData("random gif", "random gif"),
-//		tgbotapi.NewInlineKeyboardButtonData("random cat", "random cat"),
-//		tgbotapi.NewInlineKeyboardButtonData("random number", "random number"),
-//	),
-//)
-//
-//var secondLineButtons = tgbotapi.NewInlineKeyboardMarkup(
-//	tgbotapi.NewInlineKeyboardRow(
-//		tgbotapi.NewInlineKeyboardButtonData("lower number", "lower number"),
-//		tgbotapi.NewInlineKeyboardButtonData("large number", "large number"),
-//	),
-//)
