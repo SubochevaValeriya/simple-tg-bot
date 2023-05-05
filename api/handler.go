cc6f92d25c95fa9540fea640ea92bdbaf90337bc
Valeriya Subocheva <96348645+SubochevaValeriya@users.noreply.github.com>
2023-05-05 14:37:04 +0300

    initial

--- api/handler.go
+++ api/handler.go
@@ -1,22 +1,34 @@
 package api
 
 import (
+	"context"
 	"encoding/json"
 	"github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"io"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"tgBot/internal/clients/telegram"
 )
 
 var bot *tgbotapi.BotAPI
 
 func init() {
-	var err error
-	bot, err = tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	bot := telegram.NewBot(http.Client{}, os.Getenv("MYVKBOT_TOKEN"))
+	err := bot.Start(ctx)
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
 	}
+	//var err error
+	//bot, err = tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
+	//if err != nil {
+	//	log.Panic(err)
+	//}
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
@@ -27,7 +39,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Println(err)
 	}
-	log.Println("why")
+
 	var update tgbotapi.Update
 
 	err = json.Unmarshal(body, &update)
@@ -35,44 +47,57 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 		return
 	}
-	if update.Message != nil {
-		// Construct a new message from the given chat ID and containing
-		// the text that we received.
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-
-		// If the message was open, add a copy of our numeric keyboard.
-		switch update.Message.Text {
-		case "open":
-			msg.ReplyMarkup = firstLineButtons
-
-		}
-
-		// Send the message.
-		if _, err = bot.Send(msg); err != nil {
-			log.Fatal(err)
-		}
-	}
-
-	if update.Message.Text != "" {
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		//telegram.SendMsg(update, bot)
-	}
-
-	if update.CallbackQuery != nil {
-		// Respond to the callback query, telling Telegram to show the user
-		// a message with the data received.
-		callback := tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data)
-		if _, err := bot.Request(callback); err != nil {
-			log.Fatal(err)
-		}
-
-		// And finally, send a message containing the data received.
-		msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Data)
-		if _, err := bot.Send(msg); err != nil {
-			log.Fatal(err)
-		}
-	}
+	//
+	//ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	//defer stop()
+	//
+	//bot := mybot.NewBot(http.Client{}, os.Getenv("MYVKBOT_TOKEN"))
+	//err := bot.Start(ctx)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//
+	//log.Println("why")
+	//
+	//if update.Message != nil {
+	//	// Construct a new message from the given chat ID and containing
+	//	// the text that we received.
+	//	msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+	//
+	//	// If the message was open, add a copy of our numeric keyboard.
+	//	switch update.Message.Text {
+	//	case "open":
+	//		msg.ReplyMarkup = firstLineButtons
+	//
+	//	}
+	//
+	//	// Send the message.
+	//	if _, err = bot.Send(msg); err != nil {
+	//		log.Fatal(err)
+	//	}
+	//}
+	//
+	//if update.Message.Text != "" {
+	//	log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+	//
+	//	//telegram.SendMsg(update, bot)
+	//}
+	//
+	//if update.CallbackQuery != nil {
+	//	// Respond to the callback query, telling Telegram to show the user
+	//	// a message with the data received.
+	//	callback := tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data)
+	//	if _, err := bot.Request(callback); err != nil {
+	//		log.Fatal(err)
+	//	}
+	//
+	//	// And finally, send a message containing the data received.
+	//	msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Data)
+	//	if _, err := bot.Send(msg); err != nil {
+	//		log.Fatal(err)
+	//	}
+	//}
 }
 
 var firstLineButtons = tgbotapi.NewInlineKeyboardMarkup(
--- internal/clients/receiver.go
+++ internal/clients/receiver.go
@@ -1,4 +1,4 @@
-package telegram
+package clients
 
 import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 
